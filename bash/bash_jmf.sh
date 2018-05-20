6a48b424bf49c77a0414c39281a402c8c39cdb0f
Jonathan Fisher <jonathanmarkfisher@gmail.com>
2018-05-20 18:50:30 -0400

    :art: Slightly modify colors for LS on Linux

--- bash/bash_jmf.sh
+++ bash/bash_jmf.sh
@@ -40,6 +40,7 @@ alias mkdir='mkdir -p'
 case $UNAME in
     Linux)
         alias ls='ls --color'
+	export LS_COLORS="di=36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
         ;;
     Darwin)
         alias ls='ls -G'
